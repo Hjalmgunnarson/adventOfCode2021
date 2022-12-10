bdadb06150b6682247f6b82f82d2d6653afdcb4b
Hjalmgunnarson <wtvankampen@gmail.com>
2022-12-10 21:43:15 +0000

    Day7

--- src/main/scala/adventOfCode/Day7.scala
+++ src/main/scala/adventOfCode/Day7.scala
@@ -7,8 +7,21 @@ object Day7 extends App {
   val lines = Source.fromResource("day7Input.txt").getLines.toList
   val input = "\\d+".r.findAllIn(lines.head).map(_.toInt)
 
-  var states: collection.mutable.Map[Int, Int] = collection.mutable.Map.empty
+  var positions: collection.mutable.Map[Int, Int] = collection.mutable.Map.empty
+  input.foreach{ pos =>  positions.updateWith(pos) {
+    case Some(count) => Some(count + 1)
+    case None => Some(1)
+  }}
+  val positionRange = positions.keys.min to positions.keys.max
+
+def minFuelConsumption(positions: collection.mutable.Map[Int, Int], positionRange: Range, consumptionFunction: (Int, Int) => Int): Int = {
+  positionRange.map{position => positions.map{ case (p, count) => consumptionFunction(position, p) * count}.sum}.min
+}
   
-println("Stop")
+println(minFuelConsumption(positions, positionRange, simpleFuelUsage))
+println(minFuelConsumption(positions, positionRange, complexFuelUsage))
+
+def simpleFuelUsage(position: Int, p: Int): Int = (position - p).abs
+def complexFuelUsage(position: Int, crabPosition: Int): Int =  (0 to (position - crabPosition).abs).sum
 
 }
